d0c522d5400705ca05ffaa269d755e1b053d80b5
spl211 <shilonyt@post.bgu.ac.il>
2020-12-26 17:56:38 +0200

    Course

--- Server/src/main/java/Course.java
+++ Server/src/main/java/Course.java
@@ -1,16 +1,52 @@
 import java.util.Vector;
 
 public class Course {
-    private int courseNum;
-    private String courseName;
-    private Vector<Integer> kdams;
-    private int limit;
+    private final int courseNum;
+    private final String courseName;
+    private final Vector<Integer> kdams;
+    private final int limit;
     private int numOfRegistered;
 
     public Course(int courseNum_, String courseName_,
                   Vector<Integer> kdams_, int limit_){
+        courseNum = courseNum_;
+        courseName = courseName_;
+        kdams = kdams_;
+        limit = limit_;
+        numOfRegistered = 0;
     }
     //getters
 
-    //boolean registerStudent
+    public int getCourseNum() {
+        return courseNum;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public Vector<Integer> getKdams() {
+        return kdams;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public int getNumOfRegistered() {
+        return numOfRegistered;
+    }
+
+    public boolean registerStudent(){
+        boolean ans = true;
+        if(numOfRegistered == limit)
+            return false;
+        synchronized (this) {
+            numOfRegistered++;
+        }
+        return ans;
+    }
+    public synchronized void unregister(){
+        numOfRegistered--;
+    }
 }
