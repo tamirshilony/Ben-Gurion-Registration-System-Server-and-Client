ba1507f3028dfbb85e40d35298d8e8be280ed90e
OferAvin <oferavin32@gmail.com>
2020-12-29 12:24:43 +0200

    msgFactory, ack, err

--- Server/src/main/java/Ack.java
+++ Server/src/main/java/Ack.java
@@ -1,5 +1,15 @@
-package Messages;
-
 public class Ack extends Message{
-    // 2 constractor
+    private OpcodeType sourceMsgType;
+    private String optionalData = null;
+
+    // 2 constructors
+    public Ack(OpcodeType sourceMsgType){
+        super(OpcodeType.ACK);
+        this.sourceMsgType = sourceMsgType;
+    }
+    public Ack(OpcodeType sourceMsgType, String optionalData) {
+        super(OpcodeType.ACK);
+        this.sourceMsgType = sourceMsgType;
+        this.optionalData = optionalData;
+    }
 }
--- Server/src/main/java/Error.java
+++ Server/src/main/java/Error.java
@@ -1,2 +1,7 @@
 public class Error extends Message {
+    private OpcodeType sourceMsgType;
+    public Error(OpcodeType sourceMsgType){
+        super(OpcodeType.ERR);
+        this.sourceMsgType = sourceMsgType;
+    }
 }
