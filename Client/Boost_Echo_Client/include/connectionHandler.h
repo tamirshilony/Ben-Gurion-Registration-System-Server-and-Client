c5245709d67058e84a31e594911dc640630dd9d6
spl211 <shilonyt@post.bgu.ac.il>
2021-01-02 17:44:01 +0200

    g

--- Client/Boost_Echo_Client/include/connectionHandler.h
+++ Client/Boost_Echo_Client/include/connectionHandler.h
@@ -12,7 +12,7 @@ private:
 	const std::string host_;
 	const short port_;
 	boost::asio::io_service io_service_;   // Provides core I/O functionality
-	tcp::socket socket_; 
+	tcp::socket socket_;
  
 public:
     ConnectionHandler(std::string host, short port);
